6af17b80e2a2a5962d299803d11151af98b8dfa6
gwxnythh <S10262576@connect.np.edu.sg>
2023-12-01 23:41:35 +0800

    Added Comments For Other Javascripts
    more comments explaining what the code does

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,11 +1,19 @@
-// #gallery open & close img
+/* 
+  #gallery open & close img
+*/
+// add a click event listener to each image within the 'gallery' class
 document.querySelectorAll('.gallery img').forEach(image => {
+  // set the onclick function for each image
   image.onclick = () => {
+      // display the popup image container
       document.querySelector('.popup-img').style.display = 'block';
+      // set the source of the popup image to the clicked image source
       document.querySelector('.popup-img img').src = image.getAttribute('src');
   }
 });
 
+// add a click event listener to the 'span' element within the 'popup-img' class
 document.querySelector('.popup-img span').onclick = () => {
+  // hide the popup image container when the 'span' is clicked
   document.querySelector('.popup-img').style.display = 'none';
 }
--- assets/js/product.js
+++ assets/js/product.js
@@ -1,42 +1,67 @@
-// #per product img
+/* 
+  #per product img
+*/
+// function to change the source of an image with the class 'slide'
 function img(anything) {
-    document.querySelector('.slide').src = anything;
-  }
-  
-  function change(change) {
-    const line = document.querySelector('.home');
-    line.style.background = change;
-  }
-  
-  // #quantity button
-  const plus = document.querySelector(".plus"),
-    minus = document.querySelector(".minus"),
-    num = document.querySelector(".num");
-  
-  let a = 1;
-  
-  plus.addEventListener("click", () => {
-    a++;
-    a = (a < 10) ? a : 10; // Limit the quantity to 10
+  // select the image element with the class 'slide' and set its source to the provided parameter
+  document.querySelector('.slide').src = anything;
+}
+
+// function to change the background color of an element with the class 'home'
+function change(change) {
+  // select the element with the class 'home' and set its background color to the provided parameter
+  const line = document.querySelector('.home');
+  line.style.background = change;
+}
+
+/* 
+  #quantity button
+*/
+// select elements with classes 'plus', 'minus', and 'num'
+const plus = document.querySelector(".plus"),
+  minus = document.querySelector(".minus"),
+  num = document.querySelector(".num");
+
+// initialize a variable 'a' with a starting value of 1
+let a = 1;
+
+// add a click event listener to the 'plus' element
+plus.addEventListener("click", () => {
+  // increment 'a'
+  a++;
+  // limit the quantity to a maximum of 10
+  a = (a < 10) ? a : 10;
+  // update the displayed quantity in the 'num' element
+  num.innerText = (a < 10) ? "0" + a : a.toString();
+});
+
+// add a click event listener to the 'minus' element
+minus.addEventListener("click", () => {
+  // check if 'a' is greater than 1 before decrementing
+  if (a > 1) {
+    // decrement 'a'
+    a--;
+    // update the displayed quantity in the 'num' element
     num.innerText = (a < 10) ? "0" + a : a.toString();
-  });
-  
-  minus.addEventListener("click", () => {
-    if (a > 1) {
-      a--;
-      num.innerText = (a < 10) ? "0" + a : a.toString();
-    }
-  });
-  
-  
-  // #versions selector
-  const colorSelector = document.getElementById('colorSelector');
-  const productImage = document.querySelector('.main-image img');
-  
-  colorSelector.addEventListener('click', function (event) {
-    if (event.target.tagName === 'SPAN') {
-      const imgSrc = event.target.getAttribute('data-img');
-  
-      productImage.src = imgSrc;
-    }
-  });
+  }
+});
+
+
+/* 
+  #versions selector
+*/
+// select the element with the id 'colorSelector' and the element with the class 'main-image img'
+const colorSelector = document.getElementById('colorSelector');
+const productImage = document.querySelector('.main-image img');
+
+// add a click event listener to the 'colorSelector' element
+colorSelector.addEventListener('click', function (event) {
+  // check if the clicked target is a 'SPAN' element
+  if (event.target.tagName === 'SPAN') {
+    // get the value of the 'data-img' attribute from the clicked 'SPAN' element
+    const imgSrc = event.target.getAttribute('data-img');
+
+    // set the source of the 'productImage' to the obtained 'imgSrc'
+    productImage.src = imgSrc;
+  }
+});
